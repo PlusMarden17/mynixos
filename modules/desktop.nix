3e4b92e7c4dd52b9df6bc14f0e6ff9ed5d0b76d8
coconut152 <novohackijdavid857@gmail.com>
2026-03-19 19:36:00 +0200

    Update Build Script

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,7 +4,12 @@
   services.xserver = {
     enable = true;
     videoDrivers = [ "nvidia" ];
-    displayManager.lightdm.enable = true;
+    displayManager.lightdm = {
+      enable = true;
+      greeters.gtk.enable = false;
+      greeters.slick.enable = true;
+    };
+
     desktopManager.budgie.enable = true;
     xkb = {
       layout = "us,ua";
